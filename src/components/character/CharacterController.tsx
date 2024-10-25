3e6952d87582a9857bd76c91b208e65594dfe959
ADH <110677989+Alejandra-D-H@users.noreply.github.com>
2024-10-24 22:22:06 -0500

    Second game questions and design

--- src/components/character/CharacterController.tsx
+++ src/components/character/CharacterController.tsx
@@ -11,7 +11,7 @@ import { Character } from "./Character";
 import { lerpAngle } from "../../utils/utils";
 import { ROTATION_SPEED, RUN_SPEED, WALK_SPEED } from "../../utils/constants";
 import { useGame } from "../../context/GameContext";
-import Bullet from "./Bullet";
+
 
 export const CharacterController = () => {
   const { openPhishingGame, isActivePhishing, enemies, openModal, showModal } =
@@ -32,9 +32,6 @@ export const CharacterController = () => {
   const cameraLookAt = useRef<Vector3>(new Vector3());
   const [, get] = useKeyboardControls();
 
-  const [bullets, setBullets] = useState([]); // Estado para las bolitas
-  const [canShoot, setCanShoot] = useState(true);
-  const [lastShotTime, setLastShotTime] = useState(0);
 
   // Coordenada objetivo
   // const targetPosition2 = { x: -15.21, y: -6.00, z: 0.35 };
@@ -176,32 +173,6 @@ export const CharacterController = () => {
       camera.lookAt(cameraLookAt.current);
     }
 
-    const currentTime = Date.now();
-
-    // // Disparo de bolitas
-    if (rb.current && get().shoot && currentTime - lastShotTime > 2000) {
-      // 500 ms de espera entre disparos
-      // Obtener la rotación actual del personaje
-      const rotation = character.current.rotation.y; // Rotación en el eje Y
-      const direction = new Vector3(
-        Math.sin(rotation), // Calcula la dirección X
-        0, // La dirección Y será 0 porque el proyectil se mueve en el plano XZ
-        Math.cos(rotation) // Calcula la dirección Z
-      ).normalize(); // Normaliza la dirección
-
-      const position = rb.current.translation(); // Posición actual del personaje
-
-      // Agregar una nueva bolita al estado
-      setBullets((prev) => [
-        ...prev,
-        {
-          position: new Vector3(position.x, position.y, position.z),
-          direction,
-        },
-      ]);
-
-      setLastShotTime(currentTime); // Actualiza el tiempo del último disparo
-    }
   });
   // -0.1
   return (
@@ -220,17 +191,6 @@ export const CharacterController = () => {
       {/* Mostrar modal si el personaje está cerca de la coordenada */}
       {/* {showModal && <Modal onClose={() => setShowModal(false)} />} */}
 
-      {/* Renderiza las bolitas disparadas */}
-      {bullets.map((bullet, index) => (
-        <Bullet
-          key={index}
-          position={bullet.position}
-          direction={bullet.direction}
-          onRemove={() =>
-            setBullets((prev) => prev.filter((_, i) => i !== index))
-          }
-        />
-      ))}
     </>
   );
 };
--- src/components/minigames/trojan/components/TrojanWindow.tsx
+++ src/components/minigames/trojan/components/TrojanWindow.tsx
@@ -1,16 +1,51 @@
 import { useGame } from "../../../../context/GameContext";
+import Star from "./star.svg";
+import { ansOption } from "./options";
+import { useState } from "react";
 
 export const TrojanWindow = () => {
   const { closeModal } = useGame();
 
+
+  const getRandomQuestion = () => {
+    const randomIndex = Math.floor(Math.random() * ansOption.length);
+    return ansOption[randomIndex];
+  };
+
+  const [currentQuestion]= useState(getRandomQuestion());
+
   return (
-    <div className="w-2/5 h-96 bg-blue-500 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <button
-        className="text-4xl text-white border-2 border-white p-2"
-        onClick={() => closeModal()}
-      >
-        X
-      </button>
+    <div className="w-[680px] h-[240px] bg-black fixed top-1/4 right-[100px] -translate-y-1/2 rounded-md text-white text-lg ">
+      <div>
+        <div className="w-full h-80 bg-[rgb(128,128,255)] rounded-lg">
+          <button
+            className="text-4xl text-white border-2 border-white p-2"
+            onClick={() => closeModal()}
+          >
+            x
+          </button>
+          <div className="flex items-center justify-center">
+            {currentQuestion.question}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-6 mt-4">
+          {currentQuestion.options.map((option, index) => (
+            <div
+              key={index}
+              className="w-full h-[100px] bg-[rgb(128,128,255)] rounded-lg flex items-center justify-center mt-4"
+            >
+              {String.fromCharCode(97 + index)}. {option}
+            </div>
+          ))}
+        </div>
+
+        <div className="flex flex-row items-center mt-4">
+          <img src={Star} alt="Point's star" className="w-[80px] h-[80px]" />
+          <div className="w-32 h-10 bg-yellow-300 ml-2"></div>
+          <div className="w-full h-10 bg-[rgb(255,0,255)] flex justify-center items-center"></div>
+        </div>
+      </div>
     </div>
   );
 };
